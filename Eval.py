4ed82486c8c79971f370ed99ed97126b58569e56
Zhuangzhuang Dai <zhuangzhuangdai@Zhuangzhuangs-MacBook-Pro.local>
2022-03-30 20:06:30 +0800

    Done quantitative eval of ContextEmbed

--- Eval.py
+++ Eval.py
@@ -97,44 +97,25 @@ def predict_qualitative(config, sample_path, tags=None, checkpoint_path=None, ma
     print("Start Token: {}; End Token: {}; Padding: {}".format(tokenizer._cls_token, tokenizer._sep_token,
                                                                tokenizer._pad_token))
 
-    # Load Image
-    image = Image.open(sample_path)
-    # Transpose with respect to EXIF data
-    image = ImageOps.exif_transpose(image)
-    w, h = image.size
-    print("PIL Image width: {}, height: {}".format(w, h))
-    sample = coco.val_transform(image)
-    sample = sample.unsqueeze(0)
-
-    # Load skeleton caption
-    cap, cap_mask = create_caption_and_mask(start_token, config.max_position_embeddings)
-
-    # Load tag_token, tag_mask
-    if tags is not None:
-        tag_token = create_tag_token(tags)
-    else:
-        tag_token = torch.tensor([8])
-
     @torch.no_grad()
-    def evaluate():
+    def evaluate(sample_t, cap_t, cap_mask_t, tag_token_t):
         model.eval()
         decoded_batch_beams = None
 
         for i in range(config.max_position_embeddings - 1):
             if config.modality == 'ego':
-                # Note difference between ContextFuse & ContextEmbed
-                predictions = model(sample, cap, cap_mask, tag_token)
+                predictions = model(sample_t, cap_t, cap_mask_t, tag_token_t)
             else:
-                predictions = model(sample, cap, cap_mask)
+                predictions = model(sample_t, cap_t, cap_mask_t)
             predictions = predictions[:, i, :]
             predicted_id = torch.argmax(predictions, axis=-1)
 
             if predicted_id[0] == 102:
                 break
 
-            cap[:, i + 1] = predicted_id[0]
-            cap_mask[:, i + 1] = False
-        out = cap
+            cap_t[:, i + 1] = predicted_id[0]
+            cap_mask_t[:, i + 1] = False
+        out = cap_t
         '''
         ### Greedy ###
         #out, decoded_batch_beams = model.decode(sample, cap, cap_mask, beam_width=None, diverse_m=3)
@@ -143,12 +124,65 @@ def predict_qualitative(config, sample_path, tags=None, checkpoint_path=None, ma
         '''
         return out, decoded_batch_beams
 
-    output, outputs = evaluate()
+    if isinstance(sample_path, str):
+        # Load Image
+        image = Image.open(sample_path)
+        # Transpose with respect to EXIF data
+        image = ImageOps.exif_transpose(image)
+        w, h = image.size
+        print("PIL Image width: {}, height: {}".format(w, h))
+        sample = coco.val_transform(image)
+        sample = sample.unsqueeze(0)
 
-    result = tokenizer.decode(output[0].tolist(), skip_special_tokens=True)
-    print('\n' + result.capitalize() + '\n')
+        # Load skeleton caption
+        cap, cap_mask = create_caption_and_mask(start_token, config.max_position_embeddings)
 
-    cap_dict = {sample_path.split('/')[-1]: [result]}
+        # Load tag_token, tag_mask
+        if tags is not None:
+            tag_token = create_tag_token(tags)
+        else:
+            tag_token = torch.tensor([8])
+
+        output, outputs = evaluate(sample, cap, cap_mask, tag_token)
+
+        result = tokenizer.decode(output[0].tolist(), skip_special_tokens=True)
+        print('\n' + result.capitalize() + '\n')
+
+        cap_dict = {sample_path.split('/')[-1]: [result]}
+
+    elif isinstance(sample_path, list):
+        cap_dict = {}
+
+        for idx, s_path in enumerate(sample_path):
+            # Load Image
+            image = Image.open(s_path)
+            # Transpose with respect to EXIF data
+            image = ImageOps.exif_transpose(image)
+            w, h = image.size
+            print("PIL Image width: {}, height: {}".format(w, h))
+            sample = coco.val_transform(image)
+            sample = sample.unsqueeze(0)
+
+            # Load skeleton caption
+            cap, cap_mask = create_caption_and_mask(start_token, config.max_position_embeddings)
+
+            # Load tag_token, tag_mask
+            tag = tags[idx]
+            if tag is not None:
+                tag_token = create_tag_token(tag)
+            else:
+                tag_token = torch.tensor([8])
+
+            output, outputs = evaluate(sample, cap, cap_mask, tag_token)
+
+            result = tokenizer.decode(output[0].tolist(), skip_special_tokens=True)
+            print('\n' + result.capitalize() + '\n')
+            sample_dict = {s_path.split('/')[-1]: [result]}
+            cap_dict.update(sample_dict)
+
+    else:
+        raise TypeError("Sample_path invalid!")
+
     return cap_dict
 
 
