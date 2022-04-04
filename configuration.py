889e790ec4b5ce1803dcb0303b91548f7aa509d6
zdai-XPS <zdai257@gmail.com>
2022-04-04 08:56:01 +0100

    Ready for full EgoCap training

--- configuration.py
+++ configuration.py
@@ -49,11 +49,11 @@ class Config(object):
 class ConfigEgo(object):
     def __init__(self):
         # Learning Rates
-        self.lr_backbone = 1e-6
+        self.lr_backbone = 0  #1e-6
         self.lr = 1e-5
 
         # Epochs
-        self.epochs = 30
+        self.epochs = 80
         self.lr_drop = 20
         self.start_epoch = 12  # Finetune starting from 11 + 1
         self.weight_decay = 1e-4
@@ -66,9 +66,9 @@ class ConfigEgo(object):
         # Basic
         self.device = 'cuda'
         self.seed = 42
-        self.batch_size = 8
+        self.batch_size = 4
         self.num_workers = 8
-        self.checkpoint = './finetuneContextFuse.pth'
+        self.checkpoint = './finetuneContextPreFuse.pth'
         self.clip_max_norm = 0.1
 
         # Transformer
@@ -95,10 +95,10 @@ class ConfigEgo(object):
         self.pretrain_checkpoint = "./checkpoint_cl.pth"
         # Ego dataset
         self.egocap_data_dir = "/Users/zhuangzhuangdai/repos/EgoCapSurvey"
-        self.egocap_ana_filename = "analyzed_annatations_ref.json"
-        self.train_splits = [3, 4, 5, 6, 7, 8, 9, 10]
-        self.val_splits = [1, 2]
-        self.test_splits = []
+        self.egocap_ana_filename = "EgoCapAnnotations.json"  #"analyzed_annatations_ref.json"
+        self.train_splits = [4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 21]
+        self.val_splits = [3, 12, 20]
+        self.test_splits = [1, 2]
 
 
 class Config2(object):
